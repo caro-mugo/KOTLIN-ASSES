3ff5615249d861997a264b0a090f83f524b0a4ef
caro-mugo <mugonycaroline@gmail.com>
2022-04-14 09:34:43 +0300

    first commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,49 @@
+fun main(){
+    currentAccount(22333,"Carolinemugo",2000)
+    withdraw(33.3)
+    details()
+    var x=name("Banana")
+    println(x)
+
+
+
+
+}
+//1
+open class currentAccount(var accountno:Int,var accountname:String,var balance:Int){
+    fun deposit(amount:Double){
+        var y=balance+amount
+        y++
+    }
+}
+fun withdraw(amount: Double){
+    var x=0.0
+    if(x-=amount){
+        println(x)
+    }
+fun details(){
+    var y=details()
+    println(y)
+
+}
+}
+//question 3
+ data class category (var name:String,var weight:Int,var price:Int,var category:String){
+    fun groceries(product:String){
+
+
+
+    }
+}
+// question 4
+fun name(num:String):String {
+    var y = "banana"
+    num.forEach { banan ->
+        banan.banana.contentToString(){
+            println("bnn")
+        }
+
+
+    }
+return y
+}
